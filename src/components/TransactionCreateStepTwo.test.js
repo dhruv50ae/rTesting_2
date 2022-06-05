68b63d3175b3d16badf9f5ae18d13792a511fd1e
dhruv50ae <linkdp2012@gmail.com>
2022-06-05 05:50:32 +0100

    Written first test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,7 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+import userEvent from "@testing-library/user-event";
 
 test("Disabled initial pay button", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+});
+test("Enable pay button when fields are true", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
   expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
 });
